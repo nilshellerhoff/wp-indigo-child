8b8cfcfbb6ab695b6d893260c8e5e6f90c8f1b6a
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-05-10 19:24:38 +0200

    fixed theming

--- functions.php
+++ functions.php
@@ -1,9 +1,6 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( 'parenthandle' ), 
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
-    );
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles', 11);
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css');
 }
